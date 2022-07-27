e9807b31be577258670a83c61d772adc8d288702
gp622000 <ganeshpatil622000@gmail.com>
2022-07-28 01:05:41 +0530

    login

--- server.js
+++ server.js
@@ -31,5 +31,22 @@ app.get("/aboutus",(req,res)=>{
     );
 });
 
+app.get("/login",(req,res)=>{
+    res.send(
+        "<h1>Vijay sales</h1>"
+        +"</hr>"
+        +"</h3>Smart devices for sale</h3>"
+        +"<input>name"
+        +"<br>"
+        +"<ol>"
+        +"<li>laptops</li>"
+        +"<li>computers</li>"
+        +"<li>watches</li>"
+        +"<li>earphones</li>"
+        +"</ol>"
+    );
+});
+
+
 var server=app.listen(7000);
 console.log("simple running at port no 7000")
